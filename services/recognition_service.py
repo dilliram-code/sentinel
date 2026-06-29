fbac4cb7556017d0c67f69bbb51682085a15f89a
dilliram-code <chaudharydr23@gmail.com>
2026-06-29 21:56:58 +0545

    feat: implement the logic for person identification

--- services/recognition_service.py
+++ services/recognition_service.py
@@ -1,8 +1,24 @@
 import json
 import numpy as np 
 
+# calculate and get the cosine value
 def cosine_similarity(a,b):
-  
-  # return the cosine value
   return np.dot(a,b) / (np.linalg.norm(a) * np.linalg.norm(b))
 
+# identify person
+def identify_person(embedding, database_path,threshold):
+  with open(database_path) as file:
+    database = json.load(file)
+    
+  best_score = 0
+  best_person = None 
+  
+  for person in database:
+    score = cosine_similarity(embedding, np.array(person['embedding']))
+    if score > best_score:
+      best_score = score 
+      best_person = person
+  if best_score > threshold:
+    return best_person  
+  
+  return None
